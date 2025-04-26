7879aa2648c168f904efd72e308a79121e5ff841
Mohammad Saif <msaif2299@gmail.com>
2025-04-26 11:40:36 +0100

    added /formatter

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('postcss-load-config').Config} */
+
 const config = {
   plugins: {
+    'postcss-url': {
+      url: (asset) => {
+        // Only add prefix for absolute paths starting with '/'
+        if (asset.url.startsWith('/') && process.env.NODE_ENV === 'production') {
+          return `/formatter${asset.url}`;
+        }
+        return asset.url;
+      }
+    },
     tailwindcss: {},
   },
 };
