bb9f083a0f3ad35cfa706ea3f9fc45f5dea28aa6
Mohammad Saif <msaif2299@gmail.com>
2025-04-26 11:49:00 +0100

    fixed url

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -5,7 +5,7 @@ const config = {
     'postcss-url': {
       url: (asset) => {
         // Only add prefix for absolute paths starting with '/'
-        if (asset.url.startsWith('/') && process.env.NODE_ENV === 'production') {
+        if (asset.url.startsWith('/')) {
           return `/formatter${asset.url}`;
         }
         return asset.url;
